e7d3b208f5de35112f173dec520d7e42e1d2acd9
jrpbjr <jrpbjr@gmail.com>
2024-06-03 12:15:43 -0300

    Inicializando o enum de WalletType no banco de dados

--- src/main/java/com/jrpbjr/minipicpay/config/DataLoader.java
+++ src/main/java/com/jrpbjr/minipicpay/config/DataLoader.java
@@ -1,2 +1,22 @@
-package com.jrpbjr.minipicpay.config;public class DataLoader {
+package com.jrpbjr.minipicpay.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class DataLoader implements CommandLineRunner {
+
+    private final WalletTypeRepository walletTypeRepository;
+
+    public DataLoader(WalletTypeRepository walletTypeRepository) {
+        this.walletTypeRepository = walletTypeRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Arrays.stream(WalletType.Enum.values())
+                .forEach(walletType -> walletTypeRepository.save(walletType.get()));
+    }
 }
