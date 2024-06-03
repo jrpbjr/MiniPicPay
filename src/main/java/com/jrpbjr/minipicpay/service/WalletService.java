b966dd0c64f4b1c785e88971ff5846899c139436
jrpbjr <jrpbjr@gmail.com>
2024-06-03 13:11:22 -0300

    adicionando o servico WalletService

--- src/main/java/com/jrpbjr/minipicpay/service/WalletService.java
+++ src/main/java/com/jrpbjr/minipicpay/service/WalletService.java
@@ -1,2 +1,27 @@
-package com.jrpbjr.minipicpay.service;public class WalletService {
+package com.jrpbjr.minipicpay.service;
+
+import com.jrpbjr.minipicpay.controller.dto.CreateWalletDto;
+import com.jrpbjr.minipicpay.entity.Wallet;
+import com.jrpbjr.minipicpay.exception.WalletDataAlreadyExistsException;
+import com.jrpbjr.minipicpay.repository.WalletRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletService {
+
+    private final WalletRepository walletRepository;
+
+    public WalletService(WalletRepository walletRepository) {
+        this.walletRepository = walletRepository;
+    }
+
+    public Wallet createWallet(CreateWalletDto dto) {
+
+        var walletDb = walletRepository.findByCpfCnpjOrEmail(dto.cpfCnpj(), dto.email());
+        if (walletDb.isPresent()) {
+            throw new WalletDataAlreadyExistsException("CpfCnpj or Email already exists");
+        }
+
+        return walletRepository.save(dto.toWallet());
+    }
 }
