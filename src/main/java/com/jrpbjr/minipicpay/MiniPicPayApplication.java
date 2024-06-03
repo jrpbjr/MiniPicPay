1a057017d8eb2e5a72c48648680fada917521f85
jrpbjr <jrpbjr@gmail.com>
2024-06-03 11:16:27 -0300

    first commit

---
+++ src/main/java/com/jrpbjr/minipicpay/MiniPicPayApplication.java
@@ -0,0 +1,13 @@
+package com.jrpbjr.minipicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPicPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniPicPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jrpbjr/minipicpay/MiniPicPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrpbjr.minipicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPicPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
