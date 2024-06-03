d476e4a1e6ad7f8d51f8b9ccf35d7ac66bd7a275
jrpbjr <jrpbjr@gmail.com>
2024-06-03 14:08:50 -0300

    adicionando a funcionalidade de cadastro de Wallet

--- src/main/java/com/jrpbjr/minipicpay/controller/WalletController.java
+++ src/main/java/com/jrpbjr/minipicpay/controller/WalletController.java
@@ -1,2 +1,31 @@
-package com.jrpbjr.minipicpay.controller;public class WalletController {
+package com.jrpbjr.minipicpay.controller;
+
+import com.jrpbjr.minipicpay.entity.Wallet;
+
+import com.jrpbjr.minipicpay.controller.dto.CreateWalletDto;
+
+
+import com.jrpbjr.minipicpay.service.WalletService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WalletController {
+
+    private final WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @PostMapping("/wallets")
+    public ResponseEntity<Wallet> createWallet(@RequestBody @Valid CreateWalletDto dto) {
+
+        var wallet = walletService.createWallet(dto);
+
+        return ResponseEntity.ok(wallet);
+    }
 }
