5059164d1580fb468ca12a8980b9338e33be1203
jrpbjr <jrpbjr@gmail.com>
2024-06-03 13:09:12 -0300

    adicionandoi os dto´s

--- src/main/java/com/jrpbjr/minipicpay/controller/dto/CreateWalletDto.java
+++ src/main/java/com/jrpbjr/minipicpay/controller/dto/CreateWalletDto.java
@@ -1,2 +1,23 @@
-package com.jrpbjr.minipicpay.controller.dto;public class CreateWalletDto {
+package com.jrpbjr.minipicpay.controller.dto;
+
+import com.jrpbjr.minipicpay.entity.Wallet;
+import com.jrpbjr.minipicpay.entity.WalletType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CreateWalletDto(@NotBlank String fullName,
+                              @NotBlank String cpfCnpj,
+                              @NotBlank String email,
+                              @NotBlank String password,
+                              @NotNull WalletType.Enum walletType) {
+
+    public Wallet toWallet() {
+        return new Wallet(
+                fullName,
+                cpfCnpj,
+                email,
+                password,
+                walletType.get()
+        );
+    }
 }
