1fff479e7ff10f5497aed91aadf267e8c3628d08
jrpbjr <jrpbjr@gmail.com>
2024-06-03 21:21:09 -0300

    consumindo o serviço de autorização

--- src/main/java/com/jrpbjr/minipicpay/service/AuthorizationService.java
+++ src/main/java/com/jrpbjr/minipicpay/service/AuthorizationService.java
@@ -1,2 +1,27 @@
-package com.jrpbjr.minipicpay.service;public class AuthorizationService {
+package com.jrpbjr.minipicpay.service;
+
+import com.jrpbjr.minipicpay.client.AuthorizationClient;
+import com.jrpbjr.minipicpay.controller.dto.TransferDto;
+import com.jrpbjr.minipicpay.exception.PicPayException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService {
+
+    private final AuthorizationClient authorizationClient;
+
+    public AuthorizationService(AuthorizationClient authorizationClient) {
+        this.authorizationClient = authorizationClient;
+    }
+
+    public boolean isAuthorized(TransferDto transfer) {
+
+        var resp = authorizationClient.isAuthorized();
+
+        if (resp.getStatusCode().isError()) {
+            throw new PicPayException();
+        }
+
+        return resp.getBody().authorized();
+    }
 }
