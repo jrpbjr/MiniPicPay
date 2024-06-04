61e4dbdc8b9cf4dda3d737a8c4862660a978dce6
jrpbjr <jrpbjr@gmail.com>
2024-06-03 21:18:05 -0300

    adicionando dto para tranferencia

---
+++ src/main/java/com/jrpbjr/minipicpay/controller/dto/TransferDto.java
@@ -0,0 +1,11 @@
+package com.jrpbjr.minipicpay.controller.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record TransferDto(@DecimalMin("0.01") @NotNull BigDecimal value,
+                          @NotNull Long payer,
+                          @NotNull Long payee) {
+}
